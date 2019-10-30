4b5521c425d93aed290430f7f5e5bac8730990f6
ALBAN MAC <albanmac@ALBANs-MacBook-Pro.local>
2019-10-29 19:15:50 -0500

    started working on js. complted figurig out moves and setting up render

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //constants
-redStartingLocations = [2,4,6,8,
-                    9,11,13,15,
-                    18,20,22,24];
+redStartingLocations = [1,3,5,7,
+                    8,10,12,14,
+                    17,19,21,23]; //Red locations used to calculate black also
 
 
 
@@ -10,11 +10,42 @@ redStartingLocations = [2,4,6,8,
 var board;
 var playerTurn;
 class Piece {
-    constructor(color,isKing){
+    constructor(color,isKing,locationOnBoard){
         this.color = color;
         this.isKing = isKing;
+        this.locationOnBoard = locationOnBoard;
         this.possibleMoves = [];
-        //this.location
+    }
+    findPossibleMoves(){
+        let possibleMoves = [this.locationOnBoard-9,    //NW jump
+                            this.locationOnBoard-7,     //NE jump
+                            this.locationOnBoard+7,     //SW jump
+                            this.locationOnBoard+9];    //SE jump
+        //Eliminate possible moves outside of board (piece on edge)
+        //Ex: eliminates West jumps if piece on West(left) edge of board
+        if (!(this.locationOnBoard%8)) {
+            possibleMoves[0] = null;
+            possibleMoves[2] = null;
+        }
+        if (!((this.locationOnBoard+1)%8)) {
+            possibleMoves[1] = null;
+            possibleMoves[3] = null;
+        }
+        //Eliminate jump of piece moving backwards
+        if (this.color ==='red' && !this.isKing){
+            possibleMoves[0] = null;
+            possibleMoves[1] = null;
+        } else if (this.color === 'black' && !this.isKing){
+            possibleMoves[2] = null;
+            possibleMoves[3] = null;
+        }
+        //check if there is a piece at new possible move location
+        possibleMoves.forEach(function(move,index){
+            if (move){
+                if (board[move]) {possibleMoves[index] = null;}
+            }
+        });
+        return possibleMoves.filter(move => move);
     }
 }
 
@@ -33,16 +64,22 @@ boardEl.addEventListener('click',pieceEventHandler);
 
 
 //functions
-
 function init() {
     playerTurn = 'red'
     board = [];
     while (board.length < 64) { board.push(null); }
 
+    //place objects pieces on board
     redStartingLocations.forEach(function(location){
-        board[location - 1] = new Piece('red',false);
-        board[64-location] = new Piece('black',false);
+        board[location] = new Piece('red',false,location);
+        board[63-location] = new Piece('black',false,(63-location));
     });
+    //find possible moves after objects placed
+    board.forEach(function(obj,idx){
+        if (obj){
+            obj.possibleMoves = obj.findPossibleMoves();
+        }
+    })
     render();
 }
 
@@ -51,26 +88,29 @@ function pieceEventHandler(evt){
         !evt.target.hasAttribute('src')) {
             return;
         }
-    console.log(evt.target);
+    let pieceClicked = board[(parseInt(evt.target.className))];
+    // for (let i=0;)
+    console.log(pieceClicked.possibleMoves);    
 }
 
 //Maybe put this in the class, and pass in board
-function findPossibleMoves(){
+// function findPossibleMoves(){
     
-}
+// }
 
 function render(){
-    board.forEach(function(boardPiece, idx){
-        if (boardPiece === null) {
+
+    board.forEach(function(boardSpot, idx){
+        if (boardSpot === null) {
             boardSpotsEl[idx].removeAttribute('src');
-        } else if (boardPiece.color === 'black' && boardPiece.isKing) {
-            boardSpotsEl[idx].setAttribute('src',"images/BlackKing.JPG");        
-        } else if (boardPiece.color === 'black' && !boardPiece.isKing) {
-            boardSpotsEl[idx].setAttribute('src',"images/Black.JPG");        
-        } else if (boardPiece.color === 'red' && boardPiece.isKing) {
-            boardSpotsEl[idx].setAttribute('src',"images/RedKing.JPG");        
-        } else if (boardPiece.color === 'red' && !boardPiece.isKing) {
-            boardSpotsEl[idx].setAttribute('src',"images/Red.JPG");        
+        } else if (boardSpot.color === 'black' && boardSpot.isKing) {
+            boardSpotsEl[idx].setAttribute('src',"images/BlackKing.Png");        
+        } else if (boardSpot.color === 'black' && !boardSpot.isKing) {
+            boardSpotsEl[idx].setAttribute('src',"images/Black.Png");        
+        } else if (boardSpot.color === 'red' && boardSpot.isKing) {
+            boardSpotsEl[idx].setAttribute('src',"images/RedKing.Png");        
+        } else if (boardSpot.color === 'red' && !boardSpot.isKing) {
+            boardSpotsEl[idx].setAttribute('src',"images/Red.Png");        
         }
     });
     turnEl.textContent = `${playerTurn} Goes First`; //Maybe capitalize the first letter here
@@ -78,6 +118,14 @@ function render(){
 }
 
 function checkWinner(){
+
+}
+
+function movePiece(){
+    
+}
+
+function updateBoard(){
 
 }
 
